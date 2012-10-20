4e11429f6a6df7610467198edc88e5c41a1c8955
Ubuntu <ubuntu@ip-10-252-22-146.sa-east-1.compute.internal>
2012-10-20 23:39:16 +0000

    decide not to download the mp3 file

--- python/download_songs_from_xiami.py
+++ python/download_songs_from_xiami.py
@@ -66,30 +66,30 @@ br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
 # User-Agent (this is cheating, ok?)
 br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
 
-for index in range(1, 100):
-    if 'song_%d.xml' % index not in os.listdir('songs_xml'):
-        os.system('curl http://data.xiami.com/widget/xml-single/uid/0/sid/%d > songs_xml/song_%d.xml' % (index, index))
-    try:
-        tree = ET.parse('songs_xml/song_%d.xml' % index)
-        root = tree.getroot()
-        location_encoded = root[0][10].text
-        song_name = root[0][2].text
-        album_name = root[0][7].text
-        artist_name = root[0][9].text
-        album_name = album_name.replace(' ', '_')
-        artist_name = artist_name.replace(' ', '_')
-        print 'url:', location_encoded, 'artist:', artist_name, 'album:', album_name, 'song:', song_name
-        location_decoded = decode(location_encoded)
-        print 'url_decoded: ', location_decoded
-        if artist_name.encode('utf-8') not in os.listdir('songs'):
-            os.system(('mkdir songs/%s' % artist_name).encode('utf-8'))
-        if album_name.encode('utf-8') not in os.listdir(('songs/%s' % artist_name).encode('utf-8')):
-            os.system(('mkdir songs/%s/%s' % (artist_name, album_name)).encode('utf-8'))
-        #response = urllib.urlopen(location_decoded)
-        response = br.open(location_decoded)
-        html = response.read()
-        song_file = open(('songs/%s/%s/%s.mp3' % (artist_name, album_name, song_name)).encode('utf-8'), 'w')
-        song_file.write(html)
-        #os.system(('curl %s > songs/%s/%s/%s.mp3' % (location_decoded, artist_name, album_name, song_name)).encode('utf-8'))
-    except:
-        continue
+for index in range(1600, 10000):
+    if 'song_%d.xml' % index not in os.listdir('/home/ubuntu/songs_xml'):
+        os.system('curl http://data.xiami.com/widget/xml-single/uid/0/sid/%d > /home/ubuntu/songs_xml/song_%d.xml' % (index, index))
+    #try:
+    #    tree = ET.parse('/home/ubuntu/songs_xml/song_%d.xml' % index)
+    #    root = tree.getroot()
+    #    location_encoded = root[0][10].text
+    #    song_name = root[0][2].text
+    #    album_name = root[0][7].text
+    #    artist_name = root[0][9].text
+    #    album_name = album_name.replace(' ', '_')
+    #    artist_name = artist_name.replace(' ', '_')
+    #    print 'url:', location_encoded, 'artist:', artist_name, 'album:', album_name, 'song:', song_name
+    #    location_decoded = decode(location_encoded)
+    #    print 'url_decoded: ', location_decoded
+    #    if artist_name.encode('utf-8') not in os.listdir('/home/ubuntu/songs'):
+    #        os.system(('mkdir /home/ubuntu/songs/%s' % artist_name).encode('utf-8'))
+    #    if album_name.encode('utf-8') not in os.listdir(('/home/ubuntu/songs/%s' % artist_name).encode('utf-8')):
+    #        os.system(('mkdir /home/ubuntu/songs/%s/%s' % (artist_name, album_name)).encode('utf-8'))
+    #    #response = urllib.urlopen(location_decoded)
+    #    response = br.open(location_decoded)
+    #    html = response.read()
+    #    song_file = open(('/home/ubuntu/songs/%s/%s/%s.mp3' % (artist_name, album_name, song_name)).encode('utf-8'), 'w')
+    #    song_file.write(html)
+    #    #os.system(('curl %s > /home/ubuntu/songs/%s/%s/%s.mp3' % (location_decoded, artist_name, album_name, song_name)).encode('utf-8'))
+    #except:
+    #    continue

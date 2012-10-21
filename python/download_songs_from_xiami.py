3578697ae6460ba10eb2e14152956fbd5848ae51
Ubuntu <ubuntu@ip-10-252-22-146.sa-east-1.compute.internal>
2012-10-21 03:38:34 +0000

    update the dump_into_redis

--- python/download_songs_from_xiami.py
+++ python/download_songs_from_xiami.py
@@ -41,55 +41,59 @@ def decode(url_encoded):
     loc_9 = unquote(loc_8).replace('^', str(0))
     return loc_9
 
-# Browser
-br = mechanize.Browser()
+def main():
+    # Browser
+    br = mechanize.Browser()
 
-# Cookie Jar
-cj = cookielib.LWPCookieJar()
-br.set_cookiejar(cj)
+    # Cookie Jar
+    cj = cookielib.LWPCookieJar()
+    br.set_cookiejar(cj)
 
-# Browser options
-br.set_handle_equiv(True)
-br.set_handle_gzip(True)
-br.set_handle_redirect(True)
-br.set_handle_referer(True)
-br.set_handle_robots(False)
+    # Browser options
+    br.set_handle_equiv(True)
+    br.set_handle_gzip(True)
+    br.set_handle_redirect(True)
+    br.set_handle_referer(True)
+    br.set_handle_robots(False)
 
-# Follows refresh 0 but not hangs on refresh > 0
-br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+    # Follows refresh 0 but not hangs on refresh > 0
+    br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
 
-# Want debugging messages?
-#br.set_debug_http(True)
-#br.set_debug_redirects(True)
-#br.set_debug_responses(True)
+    # Want debugging messages?
+    #br.set_debug_http(True)
+    #br.set_debug_redirects(True)
+    #br.set_debug_responses(True)
 
-# User-Agent (this is cheating, ok?)
-br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+    # User-Agent (this is cheating, ok?)
+    br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
 
-for index in range(1600, 10000):
-    if 'song_%d.xml' % index not in os.listdir('/home/ubuntu/songs_xml'):
-        os.system('curl http://data.xiami.com/widget/xml-single/uid/0/sid/%d > /home/ubuntu/songs_xml/song_%d.xml' % (index, index))
-    #try:
-    #    tree = ET.parse('/home/ubuntu/songs_xml/song_%d.xml' % index)
-    #    root = tree.getroot()
-    #    location_encoded = root[0][10].text
-    #    song_name = root[0][2].text
-    #    album_name = root[0][7].text
-    #    artist_name = root[0][9].text
-    #    album_name = album_name.replace(' ', '_')
-    #    artist_name = artist_name.replace(' ', '_')
-    #    print 'url:', location_encoded, 'artist:', artist_name, 'album:', album_name, 'song:', song_name
-    #    location_decoded = decode(location_encoded)
-    #    print 'url_decoded: ', location_decoded
-    #    if artist_name.encode('utf-8') not in os.listdir('/home/ubuntu/songs'):
-    #        os.system(('mkdir /home/ubuntu/songs/%s' % artist_name).encode('utf-8'))
-    #    if album_name.encode('utf-8') not in os.listdir(('/home/ubuntu/songs/%s' % artist_name).encode('utf-8')):
-    #        os.system(('mkdir /home/ubuntu/songs/%s/%s' % (artist_name, album_name)).encode('utf-8'))
-    #    #response = urllib.urlopen(location_decoded)
-    #    response = br.open(location_decoded)
-    #    html = response.read()
-    #    song_file = open(('/home/ubuntu/songs/%s/%s/%s.mp3' % (artist_name, album_name, song_name)).encode('utf-8'), 'w')
-    #    song_file.write(html)
-    #    #os.system(('curl %s > /home/ubuntu/songs/%s/%s/%s.mp3' % (location_decoded, artist_name, album_name, song_name)).encode('utf-8'))
-    #except:
-    #    continue
+    for index in range(1600, 10000):
+        if 'song_%d.xml' % index not in os.listdir('/home/ubuntu/songs_xml'):
+            os.system('curl http://data.xiami.com/widget/xml-single/uid/0/sid/%d > /home/ubuntu/songs_xml/song_%d.xml' % (index, index))
+        #try:
+        #    tree = ET.parse('/home/ubuntu/songs_xml/song_%d.xml' % index)
+        #    root = tree.getroot()
+        #    location_encoded = root[0][10].text
+        #    song_name = root[0][2].text
+        #    album_name = root[0][7].text
+        #    artist_name = root[0][9].text
+        #    album_name = album_name.replace(' ', '_')
+        #    artist_name = artist_name.replace(' ', '_')
+        #    print 'url:', location_encoded, 'artist:', artist_name, 'album:', album_name, 'song:', song_name
+        #    location_decoded = decode(location_encoded)
+        #    print 'url_decoded: ', location_decoded
+        #    if artist_name.encode('utf-8') not in os.listdir('/home/ubuntu/songs'):
+        #        os.system(('mkdir /home/ubuntu/songs/%s' % artist_name).encode('utf-8'))
+        #    if album_name.encode('utf-8') not in os.listdir(('/home/ubuntu/songs/%s' % artist_name).encode('utf-8')):
+        #        os.system(('mkdir /home/ubuntu/songs/%s/%s' % (artist_name, album_name)).encode('utf-8'))
+        #    #response = urllib.urlopen(location_decoded)
+        #    response = br.open(location_decoded)
+        #    html = response.read()
+        #    song_file = open(('/home/ubuntu/songs/%s/%s/%s.mp3' % (artist_name, album_name, song_name)).encode('utf-8'), 'w')
+        #    song_file.write(html)
+        #    #os.system(('curl %s > /home/ubuntu/songs/%s/%s/%s.mp3' % (location_decoded, artist_name, album_name, song_name)).encode('utf-8'))
+        #except:
+        #    continue
+
+if __name__ == '__main__':
+    main()
--- python/dump_into_redis.py
+++ python/dump_into_redis.py
@@ -3,32 +3,40 @@
 import redis
 import xml.etree.ElementTree as ET
 import os
+import sys
 
+from download_songs_from_xiami import decode
+
+range1 = sys.argv[1]
+range2 = sys.argv[2]
 r = redis.Redis()
-index = 0
-for file in list('songs_xml'):
-    index += 1
-    tree = ET.parse(file)
-    root = tree.getroot()
-    location_encoded = root[0][10].text
-    location_decoded = decode(location_encoded)
-    song_name = root[0][2].text
-    album_cover = root[0][6].text
-    album_name = root[0][7].text
-    artist_name = root[0][9].text
+for file in os.listdir('/home/ubuntu/songs_xml'):
+    index1 = file.find('_')
+    index2 = file.find('.')
+    index = int(file[index1+1:index2])
+    if index >= int(range1) and index <= int(range2):
+        print file
+        tree = ET.parse('/home/ubuntu/songs_xml/'+file)
+        root = tree.getroot()
+        location_encoded = root[0][10].text
+        location_decoded = decode(location_encoded)
+        song_name = root[0][2].text.encode('utf-8')
+        album_cover = root[0][6].text
+        album_name = root[0][7].text.encode('utf-8')
+        artist_name = root[0][9].text.encode('utf-8')
 
-    if artist_name not in r.hvals('artists'):
-        r.hset('artists', len(r.hkeys('artists'), artist_name)
+        if artist_name not in r.hvals('artists'):
+            r.hset('artists', len(r.hkeys('artists')), artist_name)
 
-    if album_name not in r.hvals(artist_name):
-        r.hset(artist_name, len(r.hkeys(artist_name)), album_name)
+        if album_name not in r.hvals(artist_name):
+            r.hset(artist_name, len(r.hkeys(artist_name)), album_name)
 
-    if song_name not in r.hvals(artist_name+':'+album_name):
-        r.hset(artist_name+':'+album_name, len(r.hkeys(artist_name+':'+album_name), song_name)
+        if song_name not in r.hvals(artist_name+':'+album_name):
+            r.hset(artist_name+':'+album_name, len(r.hkeys(artist_name+':'+album_name)), song_name)
 
-        r.hset(artist_name+':'+album_name+':'+song_name, 'play_times', 0)
-        r.hset(artist_name+':'+album_name+':'+song_name, 'song_name', song_name)
-        r.hset(artist_name+':'+album_name+':'+song_name, 'album_name', album_name)
-        r.hset(artist_name+':'+album_name+':'+song_name, 'artist_name', artist_name)
-        r.hset(artist_name+':'+album_name+':'+song_name, 'album_cover', album_cover)
-        r.hset(artist_name+':'+album_name+':'+song_name, 'song_url', location_decoded)
+            r.hset(artist_name+':'+album_name+':'+song_name, 'play_times', 0)
+            r.hset(artist_name+':'+album_name+':'+song_name, 'song_name', song_name)
+            r.hset(artist_name+':'+album_name+':'+song_name, 'album_name', album_name)
+            r.hset(artist_name+':'+album_name+':'+song_name, 'artist_name', artist_name)
+            r.hset(artist_name+':'+album_name+':'+song_name, 'album_cover', album_cover)
+            r.hset(artist_name+':'+album_name+':'+song_name, 'song_url', location_decoded)
